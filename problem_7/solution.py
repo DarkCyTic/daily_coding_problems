130dc99c0dc7e72b24b805223325a847bd3148cb
Nikolay Nikolov <nikolay.g.nik@gmail.com>
2019-01-24 19:53:25 +0100

    problem_7: full solution implmented with O(n) time complexity

--- problem_7/solution.py
+++ problem_7/solution.py
@@ -1,14 +1,33 @@
 def create_fib_array(n):
     fib_array = [1, 1]
     
-    for i in range(n):
+    for i in range(n - 1):
         fib_array.append(fib_array[i] + fib_array[i + 1])
 
     return fib_array
 
 def solve(num_array):
-    fib_array = create_fib_array(len(num_array))
+    num_len = len(num_array)
+    fib_array = create_fib_array(num_len)
+
+    cur_len = 0
+    pairs = 1
+
+    for index, n in enumerate(num_array):
+        cur_len += 1
+        if index == num_len - 1:
+            pairs *= fib_array[cur_len]
+        elif (n == 2 and num_array[index + 1] > 6) or n > 2:
+            pairs *= fib_array[cur_len]
+            cur_len = 0
+
+    return pairs
 
 if __name__ == "__main__":
-    num_array = [1, 1, 1, 1, 1]
-    solve(num_array)
+    # num_array = [2, 2, 7, 2, 8]
+    raw_numbers = input("Your message: ")
+    num_array = list(map(int, list(raw_numbers)))
+    print(num_array)
+    pairs = solve(num_array)
+
+    print("Result is:", pairs)
